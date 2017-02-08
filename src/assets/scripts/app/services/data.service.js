35ba8a70b24bbeea70d5862b3f8181a75d95afc9
Harry McKillen <harry@wolfhound.ie>
2017-02-08 20:28:24 +0000

    Add new data service

---
+++ src/assets/scripts/app/services/data.service.js
@@ -0,0 +1,25 @@
+// not in use
+
+'use strict';
+
+angular.module('wolfhound.services')
+
+.factory('dataService', function($http, $q) {
+
+  return {
+    getArticles: function () {
+
+      var deferred = $q.defer();
+
+      $http.get('/assets/json/articles.json')
+        .success(function (response) {
+          deferred.resolve(response);
+        })
+        .error(function (){
+          deferred.reject();
+        });
+
+      return deferred.promise;
+    }
+  }
+ });
