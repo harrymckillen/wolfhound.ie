dd1d5c6782fe2d460248b24620d171729123480a
Harry McKillen <1691128+harrymckillen@users.noreply.github.com>
2020-10-24 17:10:24 +0100

    WIP changes

--- src/assets/scripts/app/directives/approx-reading-time.directive.js
+++ src/assets/scripts/app/directives/approx-reading-time.directive.js
@@ -1,30 +1,32 @@
 'use strict';
 
 angular.module('wolfhound.directives')
-
+.controller('approxReadingTimeCtrl')
 .directive('approxReadingTime', function () {
   return {
     restrict: 'A',
     replace: true,
     scope: true,
-    template: '<span>{{approxReadingTime}} mins read</span>',
-    controller: function ($scope){
-
-      $scope.getApproxReadingTime = function (text, average){
-
-        // remove any html tags from the string object
-        text = text.replace(/(<([^>]+)>)/ig,"");
-
-        // from what is remaining, split the string by spaces into what I determine
-        // to be a word, it's a guess. Assign it to an array.
-        var words = text.split(" ");
-
-        // divide the number of words in the array by the avgWPM, and return the rounded up value
-        return Math.ceil(words.length / average);
-      }
-
-      // assign value to scope
-      $scope.approxReadingTime = $scope.getApproxReadingTime(document.body.innerHTML, 200);
-    }
+    template: '<span>{{::approxReadingTime}} mins read</span>',
+    controller: approxReadingTimeCtrl
   }
 });
+
+function approxReadingTimeCtrl($scope, $document) {
+
+  $scope.getApproxReadingTime = function (text, average) {
+
+    // remove any html tags from the string object
+    text = text.replace(/(<([^>]+)>)/ig,"");
+
+    // from what is remaining, split the string by spaces into what I determine
+    // to be a word, it's a guess. Assign it to an array.
+    var words = text.split(" ");
+
+    // divide the number of words in the array by the avgWPM, and return the rounded up value
+    return Math.ceil(words.length / average);
+  }
+
+  // assign value to scope
+  $scope.approxReadingTime = $scope.getApproxReadingTime($document[0].body.innerHTML, 200);
+}
--- test/approx-reading-time.directive.test.js
+++ test/approx-reading-time.directive.test.js
@@ -1,23 +1,63 @@
 'use strict';
 
-describe('Approx Reading Time Directive', function() {
-  var $compile,
-      $rootScope;
+describe('Testing the Approx Reading Time Directive', function() {
+
+  var suite = null;
+
+  var documentInnerHtml = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam ex molestias sunt dicta aliquid fugit porro optio dolore praesentium ad, accusantium, ea, id. Dolor, optio quod iusto tempore natus.</p><p>Minus ipsam, minima cum fugiat, inventore consequatur, fuga ducimus eum sapiente nam nisi quam. Labore molestiae, unde veritatis quidem, ab pariatur eius quo sunt architecto possimus eveniet deserunt, aperiam sapiente.</p><p>Numquam, tempore, laborum. Minima quidem aliquid dolorum omnis vel, culpa cumque dolor ipsa assumenda sed rem unde minus soluta nihil voluptas repudiandae perferendis totam saepe, quia accusantium aliquam maxime rerum.</p><p>Quae ex, totam, soluta rerum error sequi ad nobis. Aspernatur eveniet ut accusamus ducimus libero amet recusandae sequi aliquid cumque temporibus labore ipsum, consectetur quisquam eos. Alias, eveniet quia cum.</p>';
+
+  function compileDirective(minuteValue){
+    if(minuteValue){
+      suite.scope.approxReadingTime = minuteValue;
+    }
+
+    console.log('>'+suite.scope.approxReadingTime);
+
+    var template = '<span approx-reading-time></span>';
+    suite.element = suite.$compile(template)(suite.scope)
+    suite.scope.$digest();
+  }
 
   beforeEach(module('wolfhound.app'));
 
-  beforeEach(inject(function(_$compile_, _$rootScope_){
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
+  beforeEach(inject(function($injector){
+    suite = {};
+
+    suite.$rootScope = $injector.get('$rootScope');
+    suite.scope = suite.$rootScope.$new();
+    suite.$compile = $injector.get('$compile');
+    suite.$controller = $injector.get('$controller');
   }));
 
-  it('should be defined', function() {
-    var document = {};
-    // var document.body.innerHTML = '<p>There should </p>';
+  afterEach(function(){
+    suite.scope.$destroy();
+    suite.$rootScope.$digest();
+    suite.element.remove();
+    suite = null;
+  });
 
-    var element = $compile('<span approx-reading-time></span>')($rootScope);
-    $rootScope.$digest();
-    expect(element).toBeDefined();
+  it('should be defined', function() {
+
+    compileDirective();
+    expect(suite.element).toBeDefined();
+
+  });
+
+  it('should return default "1 mins read"', function() {
+
+    compileDirective();
+
+    expect(suite.element.html()).toContain('1 mins read');
+
+  });
+
+  it('should return x value', function() {
+
+    compileDirective(5);
+
+    console.log(suite.element.html());
+
+    expect(suite.element.html()).not.toContain('5 mins read');
 
   });
 
